f99f480719b124a0a5031ea0da0c769460c8e100
GatesOfMosul <80784477+GatesOfMosul@users.noreply.github.com>
2021-03-19 20:24:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-hemlo
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
